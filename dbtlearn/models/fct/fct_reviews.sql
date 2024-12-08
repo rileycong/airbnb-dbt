c788503a91183e9225235ac0453ffb3e32682910
rileycong <chauanhcong@gmail.com>
2024-12-08 19:08:49 +1100

    Add incremental loading logic in fct_reviews model to support date range filtering

--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -15,7 +15,20 @@ SELECT
 FROM src_reviews
 WHERE review_text IS NOT NULL
 
--- append records if date is greater than the max date in the table
+/*
+if this is an incremental load (not full refresh)
+append records in the specified time range or
+if there's no time range, append from dates
+greater than the max date in the table.
+*/
+
 {% if is_incremental() %}
-  and review_date > (select max(review_date) from {{ this }})
+  {% if var("start_date", False) and var("end_date", False) %}
+    {{ log('Loading ' ~ this ~ ' incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
+    AND review_date >= '{{ var("start_date") }}'
+    AND review_date < '{{ var("end_date") }}'
+  {% else %}
+    AND review_date > (select max(review_date) from {{ this }})
+    {{ log('Loading ' ~ this ~ ' incrementally (all missing dates)', info=True)}}
+  {% endif %}
 {% endif %}
